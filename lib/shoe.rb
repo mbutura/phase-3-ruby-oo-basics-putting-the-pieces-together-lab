441ee2478e0af829daa2c44b7325e98289a6ebab
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-02-12 22:33:46 +0000

    complete

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    
+    def initialize brand
+        @brand=brand
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+    def  cobble
+        puts "Your shoe is as good as new!"
+        @condition = 'new'
+    end
+end
